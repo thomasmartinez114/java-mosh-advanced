61eba90111d2329079ccf411bc56e557bbd48826
Thomas Martinez <tmartinez114@gmail.com>
2020-04-29 00:33:24 -0400

    Types of Exceptions - Checked exception which the compiler checks at compile time

--- 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
+++ 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
@@ -1,11 +1,9 @@
 package exceptions;
 
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
-    }
-
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
+        var reader = new FileReader("file.txt");
     }
 }
