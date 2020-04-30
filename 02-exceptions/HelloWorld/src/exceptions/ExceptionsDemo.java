4667ce68bda748eea9eee73e90abde23b97ed088
Thomas Martinez <tmartinez114@gmail.com>
2020-04-29 23:44:56 -0400

    Catching Exceptions - added try catch - catch(<type of exception to catch> <give it a name - "ex">

--- 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
+++ 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
@@ -1,9 +1,15 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExceptionsDemo {
     public static void show() {
-        var reader = new FileReader("file.txt");
+        try {
+            var reader = new FileReader("file.txt");
+            System.out.println("File opened");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File does not exist");
+        }
     }
 }
