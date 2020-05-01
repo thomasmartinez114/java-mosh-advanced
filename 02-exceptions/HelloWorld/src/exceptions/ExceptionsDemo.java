84f8d1e8e76128c65cc2ea0016bb4c0ac22fcc2a
Thomas Martinez <tmartinez114@gmail.com>
2020-04-30 23:56:42 -0400

    Multiple Catch Types

--- 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
+++ 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
@@ -2,14 +2,19 @@ package exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
-        } catch (FileNotFoundException e) {
+            var value = reader.read();
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
+        catch (IOException e) {
+            System.out.println("Could not read data.");
+        }
     }
 }
