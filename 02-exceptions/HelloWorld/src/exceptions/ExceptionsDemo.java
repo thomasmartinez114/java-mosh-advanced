9de8786945dda9d84e3410fba7c81a5227d418fe
Thomas Martinez <tmartinez114@gmail.com>
2020-05-06 00:02:45 -0400

    Multiple Catches - end of the video

--- 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
+++ 02-exceptions/HelloWorld/src/exceptions/ExceptionsDemo.java
@@ -3,17 +3,17 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
             var value = reader.read();
+            new SimpleDateFormat().parse("");
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e) {
+        catch (IOException | ParseException e) {
             System.out.println("Could not read data.");
         }
     }
